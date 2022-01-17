0c52949cb5cf2380486074d58bfb1fa8c06a4b8d
Jerrall Prakash <jerrall@jerrall.com>
2022-01-16 17:08:30 -0700

    Additional Minor Changes

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 uint256 constant MALE = 0;
 uint256 constant FEMALE = 1;
 
+uint256 constant NONE = 0;
 uint256 constant FISH = 1;
 uint256 constant CAT = 2;
 uint256 constant DOG = 3;
@@ -25,45 +26,52 @@ contract PetPark {
         owner = msg.sender;
     }
 
-    function add(uint256 _animalType, uint256 _count) external {
-        require(msg.sender == owner, "Not an owner");
-        require(_animalType >= 1 && _animalType <= 6, "Invalid animal");
-        animalCounts[_animalType] = animalCounts[_animalType] + _count;
+    function add(uint256 _animalType, uint256 _count) external onlyOwner validAnimal(_animalType, "Invalid animal") {        
+        animalCounts[_animalType] += _count;
         emit Added(_animalType, _count);
     }
 
-    function borrow(uint256 _age, uint256 _gender, uint256 _animalType) external {
+    function borrow(uint256 _age, uint256 _gender, uint256 _animalType) external validAnimal(_animalType, "Invalid animal type"){
         require(_age > 0, "Invalid Age");
-        require(_gender == MALE || _gender == FEMALE, "Invalid Gender");
-        require(_animalType >= 1 && _animalType <= 6, "Invalid animal type");       
+        require(_gender == MALE || _gender == FEMALE, "Invalid Gender");            
         require(animalCounts[_animalType] > 0, "Selected animal not available");
         
         bool ageAlreadyRecorded = false;
-        if(ageOfUsers[msg.sender] != 0) {
+        if(ageOfUsers[msg.sender] != NONE) {
             require(ageOfUsers[msg.sender] == _age, "Invalid Age");
             require(genderOfUsers[msg.sender] == _gender, "Invalid Gender");
             ageAlreadyRecorded = true;
         }
 
-        require(borrowedAnimal[msg.sender] == 0, "Already adopted a pet");    
+        require(borrowedAnimal[msg.sender] == NONE, "Already adopted a pet");    
 
         if (_gender == MALE) require(_animalType == FISH || _animalType == DOG, "Invalid animal for men");
-        else require(!(_age < 40 && _animalType == CAT),"Invalid animal for women under 40");    
+        else require(!(_age < 40 && _animalType == CAT), "Invalid animal for women under 40");    
         
         if(!ageAlreadyRecorded){ 
             ageOfUsers[msg.sender] = _age;
             genderOfUsers[msg.sender] = _gender;
         }
-        animalCounts[_animalType] = animalCounts[_animalType] - 1;
+        animalCounts[_animalType]--;
         borrowedAnimal[msg.sender] = _animalType;
         emit Borrowed(_animalType);
     }
 
     function giveBackAnimal() external {
         uint256 tempBorrowedAnimal = borrowedAnimal[msg.sender];
-        require(tempBorrowedAnimal != 0, "No borrowed pets");       
-        animalCounts[tempBorrowedAnimal] = animalCounts[tempBorrowedAnimal] + 1;
-        borrowedAnimal[msg.sender] = 0;
+        require(tempBorrowedAnimal != NONE, "No borrowed pets");       
+        animalCounts[tempBorrowedAnimal]++;
+        borrowedAnimal[msg.sender] = NONE;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not an owner");
+        _;
+    }
+
+    modifier validAnimal(uint256 _animalType, string memory desiredErrorMsg) {
+        require(_animalType >= 1 && _animalType <= 6, desiredErrorMsg); 
+        _;
     }
    
 }
