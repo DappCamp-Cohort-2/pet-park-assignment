bd98ec90791248ff6730817e9c68c65f5667a63d
Warrenn Enslin <warrenn.enslin@lifelinesneuro.com>
2022-01-18 23:07:37 +0200

    Warrenn Enslin added the contract and implemented requirements

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -16,15 +16,19 @@ contract PetPark {
         Female
     }
 
+    struct PetOwner {
+        AnimalType borrowedAnimal;
+        bool hasBorrowed;
+        Gender gender;
+        uint8 age;
+    }
+
     event Added(AnimalType _animalType, uint256 _count);
     event Borrowed(AnimalType _animalType);
     event Returned(AnimalType _animalType);
 
-    address owner;
-    mapping(address => AnimalType) borrowedAnimals;
-    mapping(address => bool) hasBorrowed;
-    mapping(address => Gender) genders;
-    mapping(address => uint8) ages;
+    address private owner;
+    mapping(address => PetOwner) petOwners;
     mapping(AnimalType => uint256) public animalCounts;
 
     modifier onlyOwner() {
@@ -44,28 +48,31 @@ contract PetPark {
     }
 
     function borrow(uint8 _age, Gender _gender, AnimalType _animalType ) public {
+        PetOwner storage petOwner = petOwners[msg.sender];
+
         require(_age > 0, "Invalid Age");
-        require(!hasBorrowed[msg.sender] || genders[msg.sender] == _gender, "Invalid Gender");
-        require(!hasBorrowed[msg.sender] || ages[msg.sender] == _age, "Invalid Age");
-        require(borrowedAnimals[msg.sender] == AnimalType.None, "Already adopted a pet" );
         require(_animalType != AnimalType.None, "Invalid animal type");
+        require(!petOwner.hasBorrowed || petOwner.gender == _gender, "Invalid Gender");
+        require(!petOwner.hasBorrowed || petOwner.age == _age, "Invalid Age");
+        require(petOwner.borrowedAnimal == AnimalType.None, "Already adopted a pet" );
         require(animalCounts[_animalType] > 0, "Selected animal not available");
         require(_gender != Gender.Male || _animalType == AnimalType.Dog || _animalType == AnimalType.Fish, "Invalid animal for men");
         require(_gender != Gender.Female || _age > 40 || _animalType != AnimalType.Cat, "Invalid animal for women under 40");
 
         animalCounts[_animalType] -= 1;
-        borrowedAnimals[msg.sender] = _animalType;
-        hasBorrowed[msg.sender] = true;
-        genders[msg.sender] = _gender;
-        ages[msg.sender] = _age;
+        petOwner.borrowedAnimal = _animalType;
+        petOwner.hasBorrowed = true;
+        petOwner.gender = _gender;
+        petOwner.age = _age;
+        
         emit Borrowed(_animalType);
     }
 
     function giveBackAnimal() public {
-        AnimalType borrowedAnimal = borrowedAnimals[msg.sender];
+        AnimalType borrowedAnimal = petOwners[msg.sender].borrowedAnimal;
         require(borrowedAnimal != AnimalType.None, "No borrowed pets");
         animalCounts[borrowedAnimal] += 1;
         emit Returned(borrowedAnimal);
-        borrowedAnimals[msg.sender] = AnimalType.None;
+        petOwners[msg.sender].borrowedAnimal = AnimalType.None;
     }
 }
