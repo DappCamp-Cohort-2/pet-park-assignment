65f2c306ab6f1b0e380965ecedb9ac1905487e92
Mayuko Kondo <mayuko.kondo@outlook.com>
2022-01-18 22:05:17 +0300

    All tests passed with this contract codes!!

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -4,68 +4,63 @@ pragma solidity ^0.8.0;
 contract PetPark {
     
     address owner;
-    mapping(uint => string) public animal;
-    mapping(uint => string) public gender;
 
-    /*
-    struct Borrower {
-        uint age;
-        uint gender;
-        uint animaltype;
-    }
-    */
+    uint public constant Fish = 1;       //AnimalType: 1
+    uint public constant Cat = 2;        //AnimalType: 2
+    uint public constant Dog = 3;        //AnimalType: 3
+    uint public constant Rabbit = 4;     //AnimalType: 4
+    uint public constant Parrot = 5;     //AnimalType: 5
+    uint public constant Male = 0;       //Gender: 0
+    uint public constant Female = 1;     //Gender: 1
     
-    /*
-    mapping(address => bool) public isborrowing;
-    mapping(address => Borrower) public borrowerinfo;
-    mapping(address => uint) public borrowerindex;
-    */
-
-    uint[5] public pets;   // an array of counts of animals
-    //Borrower[] public borrowers;   // an array of borrowers 
+    mapping(uint => uint) public animalCounts;
+    mapping(address => uint) public age;
+    mapping(address => uint) public gender;
+    mapping(address => uint) public animaltype;
 
     constructor () {
         owner = msg.sender;
     }
 
-    event Added(string animaltype, uint count);
-    /*event AddedAnimal(string animaltype, uint count);
-    event Borrowed(string animaltype);
-    event Returned(string animaltype);
-    */
+    event Added(uint animaltype, uint count);
+    event Borrowed(uint animaltype);
+    event Returned(uint animaltype);
 
     // For the owner to add the count of an animal
     function add(uint _animaltype, uint _count) public {
-        require(msg.sender == owner);
-        require(_animaltype >= 1 && _animaltype <=5);
-        pets[_animaltype-1] += _count;
-        emit Added(animal[_animaltype], _count);
+        require(msg.sender == owner, "Not an owner");
+        require(_animaltype >= 1 && _animaltype <=5, "Invalid animal");
+        animalCounts[_animaltype] += _count;
+        emit Added(_animaltype, _count);
     }  
-    /*
-    //In case the owner wants to add another type of animal which 
-    //is not listed in the pet list,
-    //the owner can utilize the following function:
-    function addanimal(string memory _animal, uint _count) external {
-        require(msg.sender == owner);
-        pets.push(_count);
-        animal[pets.length] = _animal; 
-        emit AddedAnimal(animal[pets.length], _count);
+
+    // For users to borrow a pet
+    function borrow(uint _age, uint _gender, uint _animaltype) public {
+        if(age[msg.sender]!=0) {
+            require(age[msg.sender]==_age, "Invalid Age");
+            require(gender[msg.sender]==_gender, "Invalid Gender");
+        } else {
+        age[msg.sender]=_age;
+        gender[msg.sender]=_gender;
+        }
+        require(animaltype[msg.sender]==0, "Already adopted a pet");
+        require(_animaltype >= 1 && _animaltype <=5, "Invalid animal type");
+        require(_age != 0, "Invalid Age");
+        require(animalCounts[_animaltype] > 0, "Selected animal not available"); 
+        if (_gender == 0) require(_animaltype == 1 || _animaltype == 3, "Invalid animal for men");
+        else require(_age >= 40 || (_age < 40 && _animaltype != 2), "Invalid animal for women under 40");              
+        
+        animaltype[msg.sender]=_animaltype;
+        animalCounts[_animaltype] -= 1;
+        emit Borrowed(_animaltype); 
+    }
+    
+    // For borrowers to return a pet
+    function giveBackAnimal() public {
+        require(animaltype[msg.sender]!=0, "No borrowed pets");
+        animalCounts[animaltype[msg.sender]] += 1;
+        emit Returned(animaltype[msg.sender]);
+        animaltype[msg.sender]=0;       
     }
 
-    function borrow(uint _age, uint _gender, uint _animaltype) external {
-        require(isborrowing[msg.sender] == false, "You need to return a pet before borrowing a new pet.");
-        if (_gender == 0) require(_animaltype == 1 || _animaltype == 3, "Men are allowed to borrow only fish or dog.");
-        else require(_age >= 40 || (_age < 40 && _animaltype != 2), "Women under 40 are not allowed to borrow cat.");  
-        borrowers.push(Borrower(_age, _gender, _animaltype));
-        isborrowing[msg.sender] = true;
-        borrowerindex[msg.sender] = borrowers.length;
-        emit Borrowed(animal[_animaltype]);
-    }
-
-    function giveBackAnimal() external {
-        require(isborrowing[msg.sender] == true, "You haven't borrowed any pet.");
-        delete borrowers[borrowerindex[msg.sender]-1];
-        emit Returned(animal[borrowerinfo[msg.sender].animaltype]);
-    }
-    */
 }
