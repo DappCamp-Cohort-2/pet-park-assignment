98b26bdf48d1931d416f352a866610530cc2f2a4
Ryan LaBouve <imryanlabouve@gmail.com>
2022-01-18 21:22:13 -0600

    Tests for #add passing

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,7 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+// TODO: I would import differnetly if prod
+import "./open-zeplin/ownable.sol";
 
-contract PetPark {
+contract PetPark is Ownable {
+    event Added(uint256 animalType, uint256 count);
 
+    function add(uint256 _animalType, uint256 _count) public onlyOwner {
+        require(_animalType > 0, "Invalid animal");
+        // TODO: Something?
+        emit Added(_animalType, _count);
+    }
 }
