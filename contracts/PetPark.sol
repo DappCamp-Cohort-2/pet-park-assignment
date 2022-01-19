52115037330393eee80e66ca1290a57633a79309
Nolan Gelinas <nolangelinas@gmail.com>
2022-01-19 17:02:00 -0500

    Fix require error messages to conform to tests

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -18,50 +18,50 @@ contract PetPark {
     event Borrowed(uint AnimalType);
     event Returned(uint AnimalType);
 
-    constructor() public {
+    constructor() {
         owner = msg.sender;
     }
 
-    function add(uint _type, uint _count) public onlyOwner {
-        require(_type > 0 && _type <= 5, "Invalid animal");
+    function add(uint _type, uint _count) public validType(_type) onlyOwner {
         park[_type] = park[_type] + _count;
         emit Added(_type, _count);
     }
 
-    function borrow(uint _age, uint _gender, uint _type) public notBorrowing {
-        require(park[_type] > 0, "No pets of that type are in the park!");
+    function borrow(uint _age, uint _gender, uint _type) public notBorrowing validType(_type) {
+        require(_age > 0, "Invalid Age");
+        require(park[_type] > 0, "Selected animal not available");
         if(historicalBorrowers[msg.sender].age == 0 && historicalBorrowers[msg.sender].gender == 0) {
             historicalBorrowers[msg.sender] = Borrower(_age, _gender);
         }
         // Checks to make sure that the current address has not borrowed before with different age/gender
-        require(checkHistoricalBorrowers(msg.sender, _age, _gender), "You've already borrowed with different details");
+        checkHistoricalBorrowers(msg.sender, _age, _gender);
 
         // Validate age and gender data
         if(_gender == 0) {
-            require(_type == 1 || _type == 3, "Men can only borrow dogs or fish");
+            require(_type == 1 || _type == 3, "Invalid animal for men");
         }
         else if(_age < 40) {
-            require(_type != 2, "Women under 40 cannot borrow cats");
+            require(_type != 2, "Invalid animal for women under 40");
         }
-        
         park[_type] = park[_type] - 1;
         currentBorrowers[msg.sender] = true;
         emit Borrowed(_type);
     }
 
+
     function giveBackAnimal(uint _type) public {
-        require(currentBorrowers[msg.sender], "You need to borrow first!");
+        require(currentBorrowers[msg.sender], "No borrowed pets");
 
         park[_type] = park[_type] + 1;
         currentBorrowers[msg.sender] = false;
         emit Returned(_type);
     }
 
-    function checkHistoricalBorrowers(address a, uint age, uint gender) private view returns(bool) {
-        if(historicalBorrowers[a].age != age || historicalBorrowers[a].gender != gender) {
-            return false;
+    function checkHistoricalBorrowers(address a, uint age, uint gender) private view {
+        if(historicalBorrowers[a].age != 0) {
+            require(historicalBorrowers[a].age == age, "Invalid Age");
+            require(historicalBorrowers[a].gender == gender, "Invalid Gender");
         }
-        return true;
     }
 
     function animalCounts(uint _type) public view returns(uint) {
@@ -69,12 +69,17 @@ contract PetPark {
     }
 
     modifier onlyOwner {
-         require(msg.sender == owner);
+         require(msg.sender == owner, "Not an owner");
          _;
     }
 
     modifier notBorrowing {
-        require(!currentBorrowers[msg.sender], "You are already borrowing a pet!");
+        require(!currentBorrowers[msg.sender], "Already adopted a pet");
+        _;
+    }
+
+    modifier validType(uint _type) {
+        require(_type > 0 && _type <= 5, "Invalid animal");
         _;
     }
 }
