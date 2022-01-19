04168df4b07b44e10927ba62285e944794a6c63d
mardet987 <marci.detwiller@gmail.com>
2022-01-18 21:24:14 -0600

    Tested with hardhat to PetPark spec

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -25,37 +25,42 @@ contract PetPark {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
+        require(msg.sender == owner, "Not an owner");
         _;
     }
 
     function add(uint _animalType, uint _count) public onlyOwner {
-        require((_animalType < 6) && (_animalType !=0), "No animals of that type allowed!");
+        require((_animalType < 6) && (_animalType !=0), "Invalid animal");
         animalsInPark[_animalType] += _count;
         emit Added(_animalType, _count);
     }
 
     function borrow(uint _age, uint _gender, uint _animalType) public {
-        require((_animalType < 6) && (_animalType != 0), "No animals of that type known!");
-        
-        // Gender Male (0), Female (1)
-        if (_gender == 0) {
-            require((_animalType == 1) || (_animalType == 3), "Men can only borrow a dog or a fish!");
-        } else if (_age < 40) {
-            require((_animalType != 2), "Women under 40 cannot borrow a cat!");
-        }
 
         Borrower storage b = borrowers[msg.sender];
+
+        require((_animalType < 6) && (_animalType != 0), "Invalid animal type");
+        require((_age != 0), "Invalid Age");
+
         if (b.borrowed) {
-            require((b.age == _age) && (b.gender == _gender), "You're lying about age or gender!");
+            require((b.age == _age), "Invalid Age");
+            require((b.gender == _gender), "Invalid Gender");
         }
-        require((b.currentAnimal != _animalType), "You already have this animal borrowed!")
-        require((animalsInPark[_animalType] > 0), "No animal of that type available to borrow sorry!");
         
-        if (b.currentAnimal != 0) {
-            // give back an animal to borrow a new one
-            giveBackAnimal();
+        require((b.currentAnimal == 0), "Already adopted a pet");
+        // Gender Male (0), Female (1)
+        if (_gender == 0) {
+            require((_animalType == 1) || (_animalType == 3), "Invalid animal for men");
+        } else if (_age < 40) {
+            require((_animalType != 2), "Invalid animal for women under 40");
         }
+
+        require((animalsInPark[_animalType] > 0), "Selected animal not available");
+
+        // if (b.currentAnimal != 0) {
+        //     // give back an animal to borrow a new one
+        //     giveBackAnimal();
+        // }
 
         animalsInPark[_animalType] -= 1;
         b.currentAnimal = _animalType;
@@ -70,11 +75,14 @@ contract PetPark {
 
     function giveBackAnimal() public {
         Borrower storage b = borrowers[msg.sender];
-        require(b.borrowed, "You've never borrowed!");
+        require(b.borrowed, "No borrowed pets");
         animalsInPark[b.currentAnimal] += 1;
         uint returnedAnimal = b.currentAnimal;
         b.currentAnimal = 0;
         emit Returned(returnedAnimal);
     }
 
+    function animalCounts(uint _animalType) public view returns (uint) {
+        return animalsInPark[_animalType];
+    }
 }
