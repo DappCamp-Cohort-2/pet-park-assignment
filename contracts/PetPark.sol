1b452c8f4257e019b874e2b7fc1426aff3198bfe
Mimura Shunichiro <mimuramimusyun@gmail.com>
2022-01-19 14:54:17 +0100

    NIJ: add function placeholder, title, author

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,7 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
+/// @title A smart contract development exercise for Dappcamp 
+/// @author Shunichiro Mimura
 contract PetPark {
+
+    function add() public {
+
+    }
+
+    function borrow() public {
+
+    }
+
+    function giveBackAnimal() public {
+
+    }
 
 }
