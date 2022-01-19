9814f202345a1d9c95a86a6a2f63ad3299700070
Tushar Mehta <mehtatus@gmail.com>
2022-01-19 16:02:45 +0800

    Update PetPark.sol

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -3,5 +3,100 @@ pragma solidity ^0.8.0;
 
 
 contract PetPark {
+    enum AnimalType { NONE, FISH, CAT, DOG, RABBIT, PARROT }
+    enum GenderType { MALE, FEMALE }
+    mapping(AnimalType => uint) animals;
+    address public owner;
 
+    struct Borrower {
+        uint8 age;
+        GenderType gender;       
+        AnimalType animal; 
+    }
+    mapping(address => Borrower) borrowers;
+
+    // Events declaration
+    event Added(AnimalType, uint);
+    event Borrowed(AnimalType);
+    event Returned(AnimalType);
+
+    function PerPark() public {
+        // Set the transaction sender as the owner of the contract.
+        owner = msg.sender;
+
+        animals[AnimalType.FISH] = 0;
+        animals[AnimalType.CAT] = 0;
+        animals[AnimalType.DOG] = 0;
+        animals[AnimalType.RABBIT] = 0;
+        animals[AnimalType.PARROT] = 0;
+    }
+
+    // Modifier to check that the caller is the owner of the contract.
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not an owner");
+        // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to execute the rest of the code.
+        _;
+    }
+
+    function add(AnimalType _type, uint _count)
+        public
+        onlyOwner        
+    {
+        if(_type == AnimalType.NONE)
+            revert("Invalid animal");
+
+        animals[_type] += _count;
+        emit Added(_type, _count);
+    }
+
+    function borrow(uint8 _age, GenderType _gender, AnimalType _type)
+        public
+    {
+        if(_age == 0)
+            revert("Invalid Age");
+
+        if(_type == AnimalType.NONE)
+            revert("Invalid animal type");
+
+        if(animals[_type] == 0)
+            revert("Selected animal not available");
+
+        if(borrowers[msg.sender].age != 0)
+            revert("Already adopted a pet");
+
+        if(borrowers[msg.sender].gender == GenderType.MALE) {
+            if(_type != AnimalType.DOG && _type != AnimalType.FISH)
+                revert("Invalid animal for men");
+        }        
+
+        if(borrowers[msg.sender].gender == GenderType.FEMALE) {
+            if(borrowers[msg.sender].age < 40 && _type == AnimalType.CAT)
+                revert("Invalid animal for women under 40");
+        }
+
+        if(borrowers[msg.sender].age != _age)
+            revert("Invalid Age");
+        
+        if(borrowers[msg.sender].gender != _gender)
+            revert("Invalid Gender");
+
+        borrowers[msg.sender] = Borrower(_age, _gender, _type);
+        animals[_type] -= 1;
+        emit Borrowed(_type); 
+    }
+
+    function giveBackAnimal()
+        public
+    {
+        if(borrowers[msg.sender].age == 0)
+            revert("No borrowed pets");
+
+        AnimalType animal = borrowers[msg.sender].animal; 
+
+        // Reset the value to the default value.
+        delete borrowers[msg.sender];
+        animals[animal] += 1;
+        emit Returned(animal); 
+    }
 }
