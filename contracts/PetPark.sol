7caa3c3f2100e46fe2d7bc026b4dafe0712ffca0
Mimura Shunichiro <mimuramimusyun@gmail.com>
2022-01-19 15:22:02 +0100

    NIJ: implement add function

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -5,13 +5,34 @@ pragma solidity ^0.8.0;
 /// @author Shunichiro Mimura
 contract PetPark {
 
+    address owner;
+    mapping(int256 => int256) animalCount;
+
+    constructor() {
+      owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+      require(msg.sender == owner, "Not an owner");
+      _;
+    }
+
+    modifier invalidAnimal(int256 animalType) {
+      require(1 <= animalType && animalType <= 5, "Invalid animal");
+      _;
+    }
+
+   event Added(int256 animalType, int256 animalCount);
+
     /**
     Takes Animal Type and Count. Gives shelter to animals in our park.
     Only contract owner (address deploying the contract) should have access to this functionality.
     Emit event Added with parameters Animal Type and Animal Count.
     */
-    function add() public {
-
+    function add(int256 animalType, int256 count) public onlyOwner invalidAnimal(animalType) {
+        require(1 <= animalType && animalType <= 5, "Invalid animal");
+        animalCount[animalType] += count;
+        emit Added(animalType, count);
     }
 
     /**
@@ -22,7 +43,7 @@ contract PetPark {
     Throw an error if an address has called this function before using other values for Gender and Age.
     Emit event Borrowed with parameter Animal Type.
     */
-    function borrow() public {
+    function borrow(int age, int gender, int animalType) public {
 
     }
 
