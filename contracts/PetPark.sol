f5ad78da410f1c48c8bc5534a243ffc128201e42
shreyase99 <shreyase99@gmail.com>
2022-01-21 21:02:17 +0530

    enabled logging

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
+import "hardhat/console.sol";
 
 contract PetPark {
 
@@ -77,6 +77,7 @@ contract PetPark {
         borrower.animalType = _animalType;
         borrower.existing = true;
         animalCounts[_animalType] -=1;
+        console.log(animalCounts[_animalType]);
         emit Borrowed(_animalType);
     }
 
