e38fef1c503a02194d66129ba7d40f43d85e0bad
Zach Frey <zach@znfrey.com>
2022-01-21 20:37:21 -0500

    Update per review comments - use struct, change add() to external

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -18,6 +18,7 @@ contract PetPark {
         Parrot
     }
 
+
     event Added(AnimalType _type, uint256 _count);
     event Borrowed(AnimalType _type);
     event Returned(AnimalType _type);
@@ -29,11 +30,16 @@ contract PetPark {
         _;
     }
 
+    struct Borrow {
+        AnimalType species;
+        Gender  gender;
+        uint256 age;
+    }
+
     mapping (AnimalType => uint256) private _totalAnimals;
-    mapping (address => AnimalType) private _borrows;
-    mapping (AnimalType => uint256) private _borrowedAnimals;
-    mapping (address => uint) private _borrowerAge;
-    mapping (address => Gender) private _borrowerGender;
+    mapping (AnimalType => uint256) private _borrowedAnimals; // Count
+
+    mapping (address => Borrow) private _borrows;
 
     // Constructor: save owner for later
     constructor() {
@@ -45,7 +51,7 @@ contract PetPark {
               && (uint256(_type) >  uint256(AnimalType.None  ) ) );
     }
 
-    function add(AnimalType _type, uint256 _count) public onlyOwner {
+    function add(AnimalType _type, uint256 _count) external onlyOwner {
         if (!isValidAnimalType(_type)) {
             revert("Invalid animal");
         }
@@ -63,26 +69,25 @@ contract PetPark {
         address borrower = msg.sender;
 
         // Zero is the default, can use "> 0" condition to see if ever set
-        if (_borrowerAge[borrower] > 0 ) {
-            if (_borrowerAge[borrower] != _age ) {
+        if (_borrows[borrower].age > 0 ) {
+            if (_borrows[borrower].age != _age ) {
                 // Returning user has changed their age
                 revert("Invalid Age");
             }
-            if (_borrowerGender[borrower] != _gender) {
+            if (_borrows[borrower].gender != _gender) {
                 revert("Invalid Gender");
             }
         }
         else {
             // First time borrower, set age and gender
-            _borrowerAge[borrower] = _age;
-            _borrowerGender[borrower] = _gender;
+            _borrows[borrower].age = _age;
+            _borrows[borrower].gender = _gender;
         }
 
         // Can't borrow more than one animal at a time
-        if (_borrows[borrower] != AnimalType.None) {
+        if  (_borrows[borrower].species != AnimalType.None) {
             revert("Already adopted a pet");
         }
-        _borrows[borrower] = _type;
 
         // Can't borrow if no animal is available
         if (_totalAnimals[_type] == 0) {
@@ -105,19 +110,20 @@ contract PetPark {
         }
 
         _borrowedAnimals[_type]++;
+        _borrows[borrower].species = _type;
 
         emit Borrowed(_type);
     }
 
     function giveBackAnimal() public {
         address borrower = msg.sender;
-        AnimalType animalType = _borrows[borrower];
+        AnimalType animalType = _borrows[borrower].species;
         if (animalType == AnimalType.None) {
             revert("No borrowed pets");
         }
         
         // Take care of return
-        _borrows[borrower] = AnimalType.None;
+        _borrows[borrower].species = AnimalType.None;
         _borrowedAnimals[animalType]--;
         emit Returned(animalType);
     }
