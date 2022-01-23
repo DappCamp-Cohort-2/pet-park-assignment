d974d9a4e44170e2980ba88d49587c2cff332fec
brutoshi <bruce@astrox.network>
2022-01-24 01:53:35 +0800

    fix error msgs, test passed

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -19,15 +19,15 @@ contract PetPark {
 
     //types
     struct Borrower {
-        uint8 age;
+        uint age;
         Gender gender;
     }
 
     //attributes
     address public owner; 
     mapping(address => Borrower) public borrowers;
-    mapping(AnimalType => uint) public animalCounts;
-    mapping(address => AnimalType) private borrowedAnimals; 
+    mapping(AnimalType => uint) public animalCounts;  //how many animals per animal
+    mapping(address => AnimalType) private borrowedAnimals;   
 
     //events
     event Added(AnimalType _animalType, uint _count);
@@ -39,6 +39,7 @@ contract PetPark {
     }
 
     //modifiers
+    /*
     modifier onlyOwner(){
         require(owner == msg.sender, "Not an owner");
         _;
@@ -47,30 +48,43 @@ contract PetPark {
     modifier validAnimal(AnimalType _animalType){
         require(_animalType >= AnimalType.None && _animalType<=AnimalType.Rabbit, "Invalid animal type");
         _;
+    }*/
+    // modifiers
+    modifier validAnimalType(AnimalType _animalType) {
+        require(_animalType > AnimalType.None && _animalType <= AnimalType.Parrot, "Invalid animal type");
+        _;
+    }
+
+    modifier onlyOwner() {
+         if(msg.sender != owner) {
+            revert("Not an owner");
+        }
+        _;
     }
 
     //functions 
-    function add(AnimalType _animalType, uint _count) public onlyOwner validAnimal(_animalType){
+    function add(AnimalType _animalType, uint _count) external onlyOwner validAnimalType(_animalType) {
         require(_count > 0, "Count should be positive");
-
         animalCounts[_animalType] +=  _count;
         emit Added(_animalType, _count);
 
     }
 
-    function borrow(uint8 _age, Gender _gender, AnimalType _animalType) external validAnimal(_animalType){
-        Borrower memory borrower = borrowers[msg.sender];
+    function borrow(uint _age, Gender _gender, AnimalType _animalType) external validAnimalType(_animalType) {
+        Borrower storage borrower = borrowers[msg.sender];
+        if (borrower.age > 0){
+            require(borrower.age == _age, "Invalid Age");
+            require(borrower.gender == _gender, "Invalid Gender");
+        }
         require(_age > 0, "Invalid Age");
-        require(borrower.age == _age, "Invalid Age");
-        require(borrower.gender == _gender, "Invalid Gender");
-        require(borrowedAnimals[msg.sender] == AnimalType.None, "Already adopted a pet");
-        require(animalCounts[_animalType]>0, "Selected animal not available");
-    
-        
+        require(borrowedAnimals[msg.sender] == AnimalType.None, "Already adopted a pet");    
         if (_gender == Gender.Male)
             require(_animalType == AnimalType.Dog || _animalType == AnimalType.Fish, "Invalid animal for men");
-        else if (_gender == Gender.Female && _age < 40)
-            require( _animalType != AnimalType.Cat, "Invalid animal for women under 40");
+        else
+            require( _animalType != AnimalType.Cat && _age >=40, "Invalid animal for women under 40");
+
+        
+        require(animalCounts[_animalType]>0, "Selected animal not available"); 
 
         borrowers[msg.sender] = Borrower(_age, _gender);
         animalCounts[_animalType]--;
@@ -80,13 +94,13 @@ contract PetPark {
     }
 
     function giveBackAnimal() external{
-        require( borrowedAnimals[msg.sender] != AnimalType.None, "No borrowed pet");
-
         AnimalType returnedAnimal = borrowedAnimals[msg.sender];
+        require( returnedAnimal != AnimalType.None, "No borrowed pets");
+
         borrowedAnimals[msg.sender] = AnimalType.None;
         animalCounts[returnedAnimal]++;
 
         emit Returned(returnedAnimal);
-
     }
 }
+
